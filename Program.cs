5415a42bd2787224ff9228772f482c1028ca98eb
Joel Barranco <joealebarranco@gmail.com>
2023-02-13 22:07:48 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Ejercicio 1
+
+Console.WriteLine("Introduce tu nombre por favor:");
+string nombre = Console.ReadLine();
+Console.WriteLine("Tu nombre es: " + nombre);
+
+// Ejercicio 2
+var fecha = DateTime.Now;
+Console.WriteLine(fecha.ToString("T"));
